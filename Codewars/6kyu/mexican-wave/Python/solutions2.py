c395f0be4238eb649a8fee9b67bcfd885c93ea88
RevansChen <d79523@hotmail.com>
2017-08-04 00:51:22 +0800

    Update solutions2.py

--- Codewars/6kyu/mexican-wave/Python/solutions2.py
+++ Codewars/6kyu/mexican-wave/Python/solutions2.py
@@ -1,16 +1,16 @@
 def wave(str):
     result = []
-	
-	# 逐一掃描str每一個字元
+    
+    # 逐一掃描str每一個字元
     for i in range(len(str)):
         char = str[i]
-		
-		# 只對英文字母做處理
+        
+        # 只對英文字母做處理
         if char.isalpha():
-            front = str[:i] 	# 前半部的字串
-            char = char.upper()	# 目前選到的字元要變大寫
-            back = str[i+1:]	# 後半部的字串
-			
+            front = str[:i]     # 前半部的字串
+            char = char.upper() # 目前選到的字元要變大寫
+            back = str[i+1:]    # 後半部的字串
+            
             result.append(front + char + back)
-			
+            
     return result
