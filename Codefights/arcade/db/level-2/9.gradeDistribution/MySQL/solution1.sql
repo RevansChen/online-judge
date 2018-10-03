f467a2fb4e6c475703df72ea53ed2e5492660296
RevansChen <d79523@hotmail.com>
2018-10-04 03:41:09 +0800

    新增 Codefights 參考答案 Arcade Databases 9
    Arcade \ Databases \ Always Leave Table in ORDER \ 9. gradeDistribution

---
+++ Codefights/arcade/db/level-2/9.gradeDistribution/MySQL/solution1.sql
@@ -0,0 +1,14 @@
+-- MySQL
+
+CREATE PROCEDURE gradeDistribution()
+    SELECT
+        Name, ID
+    FROM
+        `Grades`
+    WHERE
+        Final > (Midterm1 * 0.25 + Midterm2 * 0.25 + Final * 0.5) AND
+        Final > (Midterm1 * 0.5 + Midterm2 * 0.5)
+    ORDER BY
+        LEFT(Name, 3),
+        ID
+    ;
