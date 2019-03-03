89e8eb937013eae59f124700897d99d7cc3124c2
RevansChen <d79523@hotmail.com>
2019-03-04 03:45:00 +0800

    新增 Codewars 參考答案 <8kyu> Beginner Series #4 Cockroach

---
+++ Codewars/8kyu/cat-years-dog-years/Python/solution1.py
@@ -0,0 +1,7 @@
+﻿# Python - 3.6.0
+
+human_years_cat_years_dog_years = lambda human_years: [
+    human_years,
+    15 + (9 if human_years > 1 else 0) + max((human_years - 2) * 4, 0),
+    15 + (9 if human_years > 1 else 0) + max((human_years - 2) * 5, 0)
+]
---
+++ Codewars/8kyu/cat-years-dog-years/Python/test.py
@@ -0,0 +1,9 @@
+﻿# Python - 3.6.0
+
+test.describe('Example Tests')
+test.it('one')
+test.assert_equals(human_years_cat_years_dog_years(1), [1, 15, 15])
+test.it('two')
+test.assert_equals(human_years_cat_years_dog_years(2), [2, 24, 24])
+test.it('ten')
+test.assert_equals(human_years_cat_years_dog_years(10), [10, 56, 64])
