25fc5feb426fea4b01113480d6319a2e46e40b66
RevansChen <d79523@hotmail.com>
2019-03-09 17:14:57 +0800

    新增 Codewars 參考答案 <8kyu> Sleigh Authentication

---
+++ Codewars/8kyu/sleigh-authentication/Python/solution1.py
@@ -0,0 +1,5 @@
+﻿# Python - 3.6.0
+
+class Sleigh(object):
+    def authenticate(self, name, password):
+       return name == 'Santa Claus' and password == 'Ho Ho Ho!'
---
+++ Codewars/8kyu/sleigh-authentication/Python/test.py
@@ -0,0 +1,15 @@
+﻿# Python - 3.6.0
+
+test.describe("Santa's Sleigh")
+
+sleigh = Sleigh()
+def test_credentials(name, password, expected):
+    Test.assert_equals(sleigh.authenticate(name, password), expected, f'Tested name {name} and password {password}')
+
+test.it('must authenticate with correct credentials')
+test_credentials('Santa Claus', 'Ho Ho Ho!', True)
+
+test.it('Must not authenticate with incorrect credentials')
+test_credentials('Santa', 'Ho Ho Ho!', False)
+test_credentials('Santa Claus', 'Ho Ho!', False)
+test_credentials('jhoffner', 'CodeWars', False)
