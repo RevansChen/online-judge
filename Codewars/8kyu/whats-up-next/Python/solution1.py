42c50d5c2b09d3715198d225ef5a3cbf559d078a
RevansChen <d79523@hotmail.com>
2019-03-11 01:32:36 +0800

    新增 Codewars 參考答案 <8kyu> What's up next?

---
+++ Codewars/8kyu/whats-up-next/Python/solution1.py
@@ -0,0 +1,11 @@
+﻿# Python - 3.6.0
+
+def next_item(xs, item):
+    try:
+        it = iter(xs)
+        for e in it:
+            if e == item:
+                return next(it)
+    except:
+        pass
+    return None
---
+++ Codewars/8kyu/whats-up-next/Python/test.py
@@ -0,0 +1,11 @@
+﻿# Python - 3.6.0
+
+@test.describe('next_item')
+def test_next_item():
+    @test.it('should pass some tests')
+    def tests():
+        Test.assert_equals(next_item([1, 2, 3, 4, 5, 6, 7, 8], 5), 6)
+        Test.assert_equals(next_item(['a', 'b', 'c'], 'd'), None)
+        Test.assert_equals(next_item(['a', 'b', 'c'], 'c'), None)
+        Test.assert_equals(next_item('testing', 't'), 'e')
+        Test.assert_equals(next_item(iter(range(1, 30000)), 12), 13)
