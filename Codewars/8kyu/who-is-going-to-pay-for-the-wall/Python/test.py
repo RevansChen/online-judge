c4cef777f1ea792fe3b6e091ed6507ba2179e3d4
RevansChen <d79523@hotmail.com>
2019-03-13 02:32:13 +0800

    新增 Codewars 參考答案 <8kyu> Who is going to pay for the wall?

---
+++ Codewars/8kyu/who-is-going-to-pay-for-the-wall/Python/test.py
@@ -0,0 +1,9 @@
+﻿# Python - 3.6.0
+
+Test.describe('Basic tests')
+Test.assert_equals(who_is_paying('Mexico'), ['Mexico', 'Me'])
+Test.assert_equals(who_is_paying('Melania'), ['Melania', 'Me'])
+Test.assert_equals(who_is_paying('Melissa'), ['Melissa', 'Me'])
+Test.assert_equals(who_is_paying('Me'), ['Me'])
+Test.assert_equals(who_is_paying(''), [''])
+Test.assert_equals(who_is_paying('I'), ['I'])
---
+++ Codewars/8kyu/who-is-going-to-pay-for-the-wall/README.md
@@ -0,0 +1,1 @@
+﻿# [Who is going to pay for the wall?](https://www.codewars.com/kata/who-is-going-to-pay-for-the-wall/)
